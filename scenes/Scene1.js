eab02f8c57fa68f24d5eed7270608da424339128
Daniel Albino <daniel18albino@gmail.com>
2020-08-18 13:28:30 +0100

    minor fixes

--- scenes/Scene1.js
+++ scenes/Scene1.js
@@ -3,15 +3,22 @@ class Scene1 extends Phaser.Scene {
     super("bootGame");
   }
   preload() {
-    this.load.image("background", "/assets/background.jpg");
-    this.load.image("ship", "./assets/ship2.png");
-    this.load.image("enemy", "./assets/enemy2.png");
-    this.load.image("enemy2", "./assets/enemy3.png");
+    this.load.image(
+      "background",
+      "space-game/tree/master/assets/background.jpg"
+    );
+    this.load.image("ship", "space-game/tree/master/assets/ship2.png");
+    this.load.image("enemy", "space-game/tree/master/assets/enemy2.png");
+    this.load.image("enemy2", "space-game/tree/master/assets/enemy3.png");
 
-    this.load.spritesheet("explosion", "./assets/boom.png", {
-      frameWidth: 16,
-      frameHeight: 16,
-    });
+    this.load.spritesheet(
+      "explosion",
+      "space-game/tree/master/assets/boom.png",
+      {
+        frameWidth: 16,
+        frameHeight: 16,
+      }
+    );
   }
 
   create() {
